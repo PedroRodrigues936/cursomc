7ae8790be4c4f4cb902c0c626d41204bd1513e88
PedroRodrigues936 <pedrolucasr936@gmail.com>
2022-04-06 18:36:23 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/pedrorodrigues/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.pedrorodrigues.cursomc.services;
+
+import java.util.Optional; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedrorodrigues.cursomc.domain.Pedido;
+import com.pedrorodrigues.cursomc.repositories.PedidoRepository;
+import com.pedrorodrigues.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id){
+		Optional<Pedido> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
+		} 
+
+}
